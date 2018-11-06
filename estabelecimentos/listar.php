2f08931120684fd09c7a926f9b6aafc59a530b94
Robson Andre <robson_x@yahoo.com.br>
2018-11-06 17:54:25 -0200

    Listar estabelecimentos OK

--- estabelecimentos/listar.php
+++ estabelecimentos/listar.php
@@ -1,25 +1,25 @@
 <?php
-	//Conectando com o banco
-	$Conn = new Conn;
-	$Qry  = new Qry; 
-	$c = $Conn->connect(HOST,USER,PASS,DB);
-	//SQL
-	$s = "SELECT indice AS uid, estabelecimento FROM ".PFIX."base_reclamacao_estabelecimento WHERE status > 0 ORDER BY estabelecimento";
-	$r = $Qry->query($s);
-	$l = $Qry->rows($r);
-	if($l){
-		$arr = $Qry->arr($r);
-		$msg[200]['results']['qtde'] = $l;
-		$msg[200]['results'] = $arr;
-		/** /
-		for($i=0; $i<count($arr); $i++){
-                    $msg[200]['results'][$i]['uid'] = $arr[$i]['uid'];
-                    $msg[200]['results'][$i]['estabelecimento'] = $arr[$i]['estabelecimento'];
-		}
-		/**/
-	}
-	
-	$output = $msg[200];
-	
-	//Desconectando o banco
-	$Conn->desconnect($c);
+//Conectando com o banco
+$Conn = new Conn;
+$Qry = new Qry;
+$c = $Conn->connect(HOST, USER, PASS, DB);
+//SQL
+$s = "SELECT indice AS uid, estabelecimento FROM " . PFIX . "base_reclamacao_estabelecimento WHERE status > 0 ORDER BY estabelecimento";
+$r = $Qry->query($s);
+$l = $Qry->rows($r);
+if ($l) {
+    $arr = $Qry->arr($r);
+    $msg[200]['results']['qtde'] = $l;
+    $msg[200]['results'] = $arr;
+    /** /
+      for($i=0; $i<count($arr); $i++){
+      $msg[200]['results'][$i]['uid'] = $arr[$i]['uid'];
+      $msg[200]['results'][$i]['estabelecimento'] = $arr[$i]['estabelecimento'];
+      }
+      /* */
+}
+
+$output = $msg[200];
+
+//Desconectando o banco
+$Conn->desconnect($c);
